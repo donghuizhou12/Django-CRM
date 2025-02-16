cb8d7b63eae6673204b8bb934edb7c772e7ca79d
Donghui Zhou <donghuizhou12@gmail.com>
2025-02-15 23:31:58 -0500

    registration

--- website/forms.py
+++ website/forms.py
@@ -4,14 +4,14 @@ from django import forms
 
 class SignupForm(UserCreationForm):
     email= forms.EmailField(Label='',widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Email Address'}))
-    first_name=forms.CharField(Label='',max_length=100,widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'First Name'}))
+    first_name=forms.CharField(Label='',max_length=100,  widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'First Name'}))
     last_name=forms.CharField(Label='',max_length=100,widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Last Name'}))
 
     class Meta:
         Model=User
         fields=('username', 'first_name','last_name', 'email','password1', 'password2')
     
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs):          
         super(SignupForm,self).__init__(*args, **kwargs)
         self.fields['username'].widget.attrs['class']='form-control'
         self.fields['username'].widget.attrs['placeholder']='User Name'
